5a32bc0c0cc41a9c0d1474b41ca0fc7ff35de7c3
Thor <thor@10.14.6>
2019-09-18 09:21:08 +0200

    opgaver uge 38

---
+++ DrawBox.java
@@ -0,0 +1,45 @@
+class DrawBox {
+  public static void main(String[] args) {
+    drawBox(6,10);
+    /*
+     * FORMÅL: 
+     * for-loops samt parametrisere printf
+     * dvs at System.out.printf("%14s%n","#") bliver til
+     * format = "%" + variabel + "s%n";
+     * myChar = "*"
+     * System.out.printf(format,myChar);
+     * 
+     *
+     * OPGAVEN:
+     * tegn en firkant vha  metoden drawBox hvor højde og bredde 
+     * angives som parametre 
+     *
+     * TESTKØRSEL:
+     * drawBox(6,10) giver flg. figur
+
+        **********
+        *        *
+        *        *
+        *        *
+        *        *
+        **********
+
+*/
+
+  }
+  public static void drawBox(int height, int width) {
+    String format = "";
+    String myEmpty = " ";
+    writeChars('*',width);
+    // TODO: skriv for-loop hvor hver iteration tegner en linje
+    // og hvor antallet af loops begrænses af height samt at
+    // width indgår i formateringen af printf.
+    writeChars('*',width);
+
+  }
+  public static void writeChars(char myChar, int frequency) {
+    //TODO: skriv for-loopet der printer myChar det antal gange
+    // der er med som anden parameter i metodekaldet
+    System.out.println();
+  }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Opgaver til uge 38 
+### Rækkefølgen 
+
+1) OrderFlowers
+
+2) ModPrime
+
+3) MakeNewSpeach
+
+4) ComputeSum
+
+5) DrawBox
+
+
+
